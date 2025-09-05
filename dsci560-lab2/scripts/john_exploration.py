b853808f9c28520301671e2aeb710c5ecc537371
johntusc <johnttra@usc.edu>
2025-09-05 15:42:04 -0400

    creating scripts to get matchups from pdf, creating matchups csv

---
+++ dsci560-lab2/scripts/john_exploration.py
@@ -0,0 +1,49 @@
+import pandas as pd
+import pdfplumber
+
+def get_matchups(text):
+    home_v_away = []
+
+    keywords = {'El':'El Savador', 'Savador':'El Savador', 
+                'Trinidad':'Trinidad & Tobago', 'Tobago':'Trinidad & Tobago',
+                'Costa':'Costa Rica', 'Rica':'Costa Rica'}
+
+    for line in text.split('\n'):
+        line = line.split()
+        if 'V' in line:
+            index = line.index('V')
+            left, right = line[index - 1], line[index + 1]
+            if left in keywords:
+                home = keywords[left]
+            else:
+                home = left
+            if right in keywords:
+                away = keywords[right]
+            else:
+                away = right
+            home_v_away.append([home, away])
+    
+    return home_v_away
+
+def matchups_to_df(matchups_list):
+    df = pd.DataFrame(matchups_list, columns=['Home', 'Away'])
+    return df
+
+if __name__ == "__main__":
+    file = 'concacaf_qualifiers.pdf'
+    path = '../data/' + file
+
+    with pdfplumber.open(path) as pdf:  
+        page = pdf.pages[0]
+        text = page.extract_text()
+        print(f'Loading pdf: {file}\n')
+
+    matchups = get_matchups(text)
+    df = matchups_to_df(matchups)
+
+    print('Extractring matchups\n')
+    print(f'Converting {file} to .csv\n')
+
+    df.to_csv('../data/concacaf_matchups.csv', index=False)
+
+    print('CSV file saved to data folder')    
